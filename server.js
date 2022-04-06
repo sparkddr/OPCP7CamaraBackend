7c29ed49e1dcb14416ed2057d87b833da405eb4a
spark_d <camara.camille@gmail.com>
2022-04-06 12:00:15 +0200

    Installation de express

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const http = require('http')
+const http = require('http');
+const app = require('./app');
 
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
-
-const server = http.createServer((req, res) => {
-res.end('Voilà la réponse du serveur ! ');
-});
-
-server.listen(process.env.PORT  || 3000) ;
+server.listen(process.env.PORT || 3000);
